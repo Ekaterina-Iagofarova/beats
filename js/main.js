7ca523728753f9fc8bcf1e98db9e9923696a7938
Ekaterina Yagofarova <k.yags@yandex.ru>
2020-08-29 01:44:15 +0300

    fixed slider

--- js/main.js
+++ js/main.js
@@ -32,41 +32,35 @@ console.log(sliderList);
 let sliderPosition = 0;
 
 
-
-// обработчики нажатия правых стрелок
 arrowsRight.forEach((arrow) => {
     arrow.addEventListener('click', () => moveRight());
 });
 
-// обработчики нажатия левых стрелок
+
 arrowsLeft.forEach((arrow) => {
     arrow.addEventListener('click', () => moveLeft());
 });
 
 
-
-
-// метод реализующий логику, для правых стрелок
 function moveRight() {
     sliderPosition+= slideWidth.clientWidth;
-    // если мы в конечной позиции слайдера, то переходим на последний слайд
+
     console.log(sliderPosition);
     console.log(sliderList.scrollWidth);
     if (sliderPosition === sliderList.scrollWidth){
 
-        // переходим к первому слайду
         sliderPosition = 0;
     }
     rootSlider.style.transform = `translate(-${sliderPosition}px)`;
 }
-// метод реализующий логику, для левых стрелок
+
 function moveLeft() {
     if (sliderPosition === 0){
-        // если мы в начальной позиции слайдера, то переходим на последний слайд
+
         sliderPosition = sliderList.scrollWidth - slideWidth.clientWidth;
     }
     else{
-        // просто сдвигаем, на велину слайда
+
         sliderPosition -= slideWidth.clientWidth;
     }
 
